8e07dd514d3d735860b51fdc7e02a01a4fd10fce
Aryan1336 <aryanojha2006@gmail.com>
2026-04-22 18:25:07 +0530

    Create sample_data.sql

---
+++ sample_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO Farmer VALUES (1,'Aryan Ojha','9876543210','Delhi');
+INSERT INTO Field VALUES (101,1,'North Farm',5.5);
+INSERT INTO Sensor VALUES (201,101,'Moisture','Active');
+INSERT INTO Sensor_Data VALUES (1,201,45.7,NOW());
